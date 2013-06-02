32ae2379a3b8f4f28fc3fbee594a8c5c3e262179
Caylan Larson <i@caylan.net>
2013-06-01 19:01:04 -0500

    Initial Commit

---
+++ Text Effect/SemiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SemiAppDelegate.h
+//  Text Effect
+//
+//  Created by Caylan Larson on 6/1/13.
+//  Copyright (c) 2013 Caylan Larson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SemiAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Text Effect/SemiAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SemiAppDelegate.m
+//  Text Effect
+//
+//  Created by Caylan Larson on 6/1/13.
+//  Copyright (c) 2013 Caylan Larson. All rights reserved.
+//
+
+#import "SemiAppDelegate.h"
+
+@implementation SemiAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
